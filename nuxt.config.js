93a5363ed8835284bf71ffd34219615d11a2b3ca
Max Schmidt <max.schmidt@outlook.de>
2020-05-23 16:14:04 +0200

    added matomo

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,7 @@ export default {
    */
   head: {
     title: "M&R Werk",
+    __dangerouslyDisableSanitizers: ["script"],
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
@@ -21,7 +22,15 @@ export default {
           "Das M&R Werk Iserlohn bietet den Komplettservice im Bereich Schrott & Altmetall Handel. Wir holen deinen Schrott ab und recyclen in fachgerecht und nachhaltig. Das beinhaltet auch die Demontage, sowie die Verwiegung von Metallen vor Ort. Alternativ bieten wir für deinen Schrott  mobile Gitterboxen die auf Abruf von uns getauscht werden. Ruf uns unverbindlich an und lass dich beraten! Dein Schrott gehört noch lange nicht zum alten Eisen!"
       }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    script: [
+      {
+        innerHTML:
+          "var _paq = window._paq || [];_paq.push(['trackPageView']);_paq.push(['enableLinkTracking']);(function() {var u='//matomo.becurly.de/';_paq.push(['setTrackerUrl', u+'matomo.php']);_paq.push(['setSiteId', '2']);var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);})();",
+        type: "text/javascript",
+        charset: "utf-8"
+      }
+    ]
   },
   /*
    ** Customize the progress-bar color
